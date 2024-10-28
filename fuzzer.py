2830bdac5c80d663248cfe4738d643263cb1b172
N1rt <129163159+N1rt@users.noreply.github.com>
2024-10-27 22:15:26 -0300

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,10 +1,17 @@
-#!/bin/python3
-
 import requests
 import sys 
-import os
+import time
 
-os.system("figlet Fuzz3r by N1r7")
+title =  """
+_____              _____        _             _   _ _     _____ 
+|  ___|   _ _______|___ / _ __  | |__  _   _  | \ | / |_ _|___  |
+| |_ | | | |_  /_  / |_ \| '__| | '_ \| | | | |  \| | | '__| / / 
+|  _|| |_| |/ / / / ___) | |    | |_) | |_| | | |\  | | |   / /  
+|_|   \__,_/___/___|____/|_|    |_.__/ \__, | |_| \_|_|_|  /_|   
+                                       |___/                   
+"""
+
+print(title,sep='')
 
 if(len(sys.argv) < 3 and len(sys.argv) > 1):
     print("ParameterError:fuzzer takes 2 arguments")
@@ -24,9 +31,14 @@ if(len(sys.argv) == 3):
                 wordlist_lines[line] = wordlist_lines[line].replace("\n","")
     except:
         print("FileNotFounded:Error,WordList not founded,do you put the right path?")
+
+    #testando se o site esta funcionando
+    r_test = requests.get(Site)
+    print(f'testando se a conexão com o site está funcionando...\n[SiteRequest]:{Site}\n[StatusCode]:{r_test.status_code}')
+    time.sleep(5)
+    print("iniciando fuzzer...")
     for line in wordlist_lines:
         r = requests.get(f"{Site}{line}")
         print(f"PATH:{Site}{line}\nStatus Code:{r.status_code}")
     print("The program has ended.")
-
 
